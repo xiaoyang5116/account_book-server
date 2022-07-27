770807f4edd3a6756fe330ec0a292859a63c6888
shawn <1443584075@qq.com>
2022-07-27 14:04:51 +0800

    添加获取用户信息接口

--- app/controller/user.js
+++ app/controller/user.js
@@ -119,6 +119,26 @@ class UserController extends Controller {
     }
   }
 
+  // 获取用户信息
+  async getUserInfo() {
+    const { ctx, app } = this
+    const token = ctx.request.header.authorization
+    // 通过 app.jwt.verify 方法，解析出 token 内的用户信息
+    const decode = app.jwt.verify(token, app.config.secret)
+    // 通过 getUserByName 方法，以用户名 decode.username 为参数，从数据库获取到该用户名下的相关信息
+    const userInfo = await ctx.service.user.getUserByName(decode.username)
+    // userInfo 中应该有密码信息，所以我们指定下面四项返回给客户端
+    ctx.body = {
+      code: 200,
+      msg: "请求成功",
+      data: {
+        id: userInfo.id,
+        username: userInfo.username,
+        signature: userInfo.signature,
+        avatar: userInfo.avatar
+      }
+    }
+  }
 }
 
 module.exports = UserController
--- app/router.js
+++ app/router.js
@@ -12,6 +12,8 @@ module.exports = app => {
   router.post('/api/user/register', controller.user.register);
   // 登陆
   router.post('/api/user/login', controller.user.login);
+  // 获取用户信息
+  router.get('/api/user/get_userinfo', _jwt, controller.user.getUserInfo);
   // 验证 token
   router.get('/api/user/test', _jwt, controller.user.test);  // 放入第二个参数，作为中间件过滤项
 
